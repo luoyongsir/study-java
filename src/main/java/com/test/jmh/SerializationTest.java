5f2bb70fb3f395dbaea5d63f3444ee637da5b07a
罗勇 <luoyongsir@gmail.com>
2022-09-06 16:00:34 +0800

    升级 fastjson

--- src/main/java/com/test/jmh/SerializationTest.java
+++ src/main/java/com/test/jmh/SerializationTest.java
@@ -1,6 +1,6 @@
 package com.test.jmh;
 
-import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson2.JSON;
 import com.test.utils.JacksonUtil;
 import com.test.utils.ProtoStuffUtil;
 import com.test.vo.BootPopup;
